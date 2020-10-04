2ab4a22628b39a24a50d4d40d684f3f2f907423c
Lester Lin <lesterlin88@gmail.com>
2020-10-04 18:14:14 +0800

    included SE logic for forming lateral structured triangulation

--- SE_models/extrusion_mode.cmd
+++ SE_models/extrusion_mode.cmd
@@ -268,3 +268,145 @@ for ( bubase := 2 ; bubase < major ; bubase := bubase + 2 ) {
 
 }; // end of 'buildbasal' command
 buildbasal;
+
+//****************************************************************************************************
+// Build lateral
+buildlat := {
+
+	// Note: use 'bulat' to determine the height and z-interval of each band
+	for ( bulat := 2 ; bulat <= 20 ; bulat := bulat + 2 ){
+
+		// to count the current total number of bands excluding the basal pole
+		band_max := max(edge elsa where (elsa.cbandwidth > 0), elsa.cbandwidth);
+		ring_max := max(edge elsa where (elsa.cringedge > 0), elsa.cringedge);
+		tri_max := max(facet ff where (ff.tri > 0), ff.tri);
+
+		// create new lateral vertices
+		for ( bb := 1 ; bb < (discret_max+1) ; bb := bb + 1 ) {
+		new_cringvertex := new_vertex((major)*cos(bb*pi/discret_half),
+									 (minor)*sin(bb*pi/discret_half),
+									 (bulat)
+									 );
+		set vertex[new_cringvertex] v_side bb;
+		set vertex[new_cringvertex] cringvertex (ring_max + 1);
+	};
+
+		// Loop to build edges
+		for ( bb := 1 ; bb < (discret_max+1) ; bb := bb + 1 ) {
+
+		if (bb == discret_max) then {
+			// vertical
+			foreach vertex vv where (cringvertex == (ring_max) && v_side == bb) do {
+				foreach vertex vvv where (cringvertex == (ring_max + 1) && v_side == bb) do {
+					new_vertical := new_edge(vv.id, vvv.id);
+					set edge[new_vertical] cbandwidth (band_max + 1);
+					set edge[new_vertical] e_side bb;
+				};
+			};
+			// diagonal edge for new band, pointing from top right to bottom left
+			foreach vertex vv where (cringvertex == (ring_max + 1) && v_side == 1) do {
+				foreach vertex vvv where (cringvertex == ring_max && v_side == bb) do {
+					new_diag := new_edge(vv.id, vvv.id);
+					set edge[new_diag] e_diag 1;
+					set edge[new_diag] e_side bb;
+					set edge[new_diag] bandsq (band_max + 1);
+				};
+			};
+			// new cringedge
+			foreach vertex vv where (cringvertex == (ring_max + 1) && v_side == bb) do {
+				foreach vertex vvv where (cringvertex == (ring_max + 1) && v_side == 1) do {
+					new_horizontal := new_edge(vv.id, vvv.id);
+					set edge[new_horizontal] cringedge (ring_max + 1);
+					set edge[new_horizontal] e_side bb;
+				};
+			};
+		}
+		else {
+			// vertical
+			foreach vertex vv where (cringvertex == (ring_max) && v_side == bb) do {
+				foreach vertex vvv where (cringvertex == (ring_max + 1) && v_side == bb) do {
+					new_vertical := new_edge(vv.id, vvv.id);
+					set edge[new_vertical] cbandwidth (band_max + 1);
+					set edge[new_vertical] e_side bb;
+				};
+			};
+			// diagonal edge for new band, pointing from top right to bottom left
+			foreach vertex vv where (cringvertex == (ring_max + 1) && v_side == (bb+1)) do {
+				foreach vertex vvv where (cringvertex == ring_max && v_side == bb) do {
+					new_diag := new_edge(vv.id, vvv.id);
+					set edge[new_diag] e_diag 1;
+					set edge[new_diag] e_side bb;
+					set edge[new_diag] bandsq (band_max + 1);
+				};
+			};
+			// new cringedge
+			foreach vertex vv where (cringvertex == (ring_max + 1) && v_side == bb) do {
+				foreach vertex vvv where (cringvertex == (ring_max + 1) && v_side == (bb+1)) do {
+					new_horizontal := new_edge(vv.id, vvv.id);
+					set edge[new_horizontal] cringedge (ring_max + 1);
+					set edge[new_horizontal] e_side bb;
+				};
+			};
+		}; // end of else for edge
+	}; // end of building edges
+
+		// Loop to build facets
+		for ( bb := 1 ; bb < (discret_max+1) ; bb := bb + 1 ) {
+
+		if (bb == discret_max) then {
+		// Right triangle on new band
+		foreach edge ee1 where (cbandwidth == (band_max + 1) && e_side == 1) do {
+			foreach edge ee2 where (bandsq == (band_max + 1) && e_side == bb) do {
+				foreach edge ee3 where (cringedge == (ring_max) && e_side == bb) do {
+					new_right_tri := new_facet(ee1.id, ee2.id, ee3.id);
+					set facet[new_right_tri] tri (band_max + 1);
+					set facet[new_right_tri] f_side bb;
+				};
+			};
+		};
+		// Left triangle on new band
+		foreach edge ee1 where (bandsq == (band_max + 1) && e_side == bb) do {
+			foreach edge ee2 where (cringedge == (ring_max + 1) && e_side == bb) do {
+				foreach edge ee3 where (cbandwidth == (band_max + 1) && e_side == bb) do {
+					new_left_tri := new_facet(-ee1.id, -ee2.id, -ee3.id);
+					set facet[new_left_tri] tri (band_max + 1);
+					set facet[new_left_tri] f_side bb;
+				};
+			};
+		};
+		}
+		else {
+		// Right triangle on new band
+		foreach edge ee1 where (cbandwidth == (band_max + 1) && e_side == (bb+1)) do {
+			foreach edge ee2 where (bandsq == (band_max + 1) && e_side == bb) do {
+				foreach edge ee3 where (cringedge == (ring_max) && e_side == bb) do {
+					new_right_tri := new_facet(ee1.id, ee2.id, ee3.id);
+					set facet[new_right_tri] tri (band_max + 1);
+					set facet[new_right_tri] f_side bb;
+				};
+			};
+		};
+		// Left triangle on new band
+		foreach edge ee1 where (bandsq == (band_max + 1) && e_side == bb) do {
+			foreach edge ee2 where (cringedge == (ring_max + 1) && e_side == bb) do {
+				foreach edge ee3 where (cbandwidth == (band_max + 1) && e_side == bb) do {
+					new_left_tri := new_facet(-ee1.id, -ee2.id, -ee3.id);
+					set facet[new_left_tri] tri (band_max + 1);
+					set facet[new_left_tri] f_side bb;
+				};
+			};
+		};
+	}; // end of else statement for building facets
+	}; // end of loop for building facets
+
+}; // end of 'bulat' for-loop
+
+	// to count the current total number of bands excluding the basal pole
+	band_max := max(edge elsa where (elsa.cbandwidth > 0), elsa.cbandwidth);
+	ring_max := max(edge elsa where (elsa.cringedge > 0), elsa.cringedge);
+	tri_max := max(facet ff where (ff.tri > 0), ff.tri);
+
+}; // end of 'buildlat' command
+buildlat;
+
+//****************************************************************************************************
